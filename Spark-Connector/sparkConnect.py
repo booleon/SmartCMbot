5109b0566154c51e46e8631e44a6610bb1b7069c
booleon <kde@florimond.eu>
2016-12-10 15:15:54 +0100

    remove echobot

--- Spark-Connector/sparkConnect.py
+++ Spark-Connector/sparkConnect.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 
 import requests
+import json
 from secret import *
+from ciscosparkapi import CiscoSparkAPI
 
-url = "https://api.ciscospark.com/v1/messages"
+class sparkconnector :
 
-payload = "{\r\n  \"toPersonEmail\" : \"cedric.bonhomme@metapolis.fr\",\r\n  \"text\" : \"This message is sent to a person directly => will create a 1-1 room, and make you rejoin a pre-existing 1-1 room you've chosen to leave\"\r\n}"
-headers = {
-    'authorization': "Bearer " + SPARK_DEV_KEY + "",
-    'content-type': "application/json",
-    'cache-control': "no-cache",
-    'postman-token':  POSTMAN_KEY
-    }
-
-response = requests.request("POST", url, data=payload, headers=headers)
-
-print(response.text)
+    def __init__(self, mykey=SPARK_DEV_KEY) :
+        api = CiscoSparkAPI(access_token=mykey)
+        decoded = json.loads(api.people.me())
+        print("now connected as" + decoded['displayName'])
