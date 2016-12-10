b6514cd8835ce96f9aa3f5e36ed61028cc79f7f1
booleon <kde@florimond.eu>
2016-12-10 18:24:59 +0100

    comunication to cisco spark works !

--- Spark-Connector/sparkConnect.py
+++ Spark-Connector/sparkConnect.py
@@ -8,6 +8,25 @@ from ciscosparkapi import CiscoSparkAPI
 class sparkconnector :
 
     def __init__(self, mykey=SPARK_DEV_KEY) :
-        api = CiscoSparkAPI(access_token=mykey)
-        decoded = json.loads(api.people.me())
-        print("now connected as" + decoded['displayName'])
+        self.api = CiscoSparkAPI(access_token=mykey)
+        user = self.api.people.me()
+        print("Moderator connected as " + user.displayName)
+        self.cleanRooms("smartCMbot_admin")
+        print('Old Room removed')
+        self.adminroom = self.api.rooms.create("smartCMbot_admin")
+        print("Room smartCMbot_admin created")
+
+    def sendToRoom(self, message) :
+        self.api.messages.create(self.adminroom.id,text=message)
+
+    def cleanRooms(self, room_name) :
+        all_rooms = self.api.rooms.list()
+        old_rooms = [room for room in all_rooms if room_name in room.title]
+
+        # Delete all of the demo rooms
+        for room in old_rooms:
+            self.api.rooms.delete(room.id)
+
+if __name__ == "__main__" :
+    connect = sparkconnector()
+    connect.sendToRoom('hello world')
